0c8c128ecf1c283c4df01430ede2e52c49fce1ae
seplusi <seplusi_arcanjo@hotmail.com>
2020-11-06 17:49:18 +0100

    Update test_common.py

--- src/common/test_common.py
+++ src/common/test_common.py
@@ -7,6 +7,8 @@ class TestCommon:
     def __init__(self, driver):
         self.driver = driver
 
-    def find_element(self, by_locator, selector, timeout=10):
-        WebDriverWait(self.driver, timeout).until(EC.element_to_be_clickable((by_locator, selector)))
-        return self.driver.find_element(by_locator, selector)
+    def find_element(self, by_locator, timeout=10):
+        return WebDriverWait(self.driver, timeout).until(EC.element_to_be_clickable((by_locator)))
+
+    def enter_text(self, by_locator, text):
+        return WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(by_locator)).send_keys(text)
